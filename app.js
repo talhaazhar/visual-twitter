96b40e02241fc60b044628473659cebe4443fd26
Talha Azhar <txa2279@rit.edu>
2018-09-28 14:10:35 -0400

    added twitter auth container file

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-var express = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+var config = require("./config")
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
---
+++ auth.js
@@ -0,0 +1,9 @@
+var config = {
+	data:{
+		consumer_key: "YOUR-CONSUMER-KEY",
+		consumer_secret: 'YOUR CONSUMER SECRET',
+		token: 'YOUR TOKEN',
+		token_secret: 'YOUR TOKEN SECRET'
+	}
+}
+module.exports = config;
