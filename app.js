f05386a85e40bc2f5678c2cb159306945e68c005
Talha Azhar <txa2279@rit.edu>
2018-10-01 16:22:17 -0400

    added json and twit api

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
+var Twit = require('twit')
+var auth = require("./auth")
+
+
 const app = express()
 const port = 3000
-var config = require("./config")
+
+
+var T = new Twit({
+    consumer_key:         auth.data.consumer_key,
+    consumer_secret:      auth.data.consumer_secret,
+    access_token:         auth.data.access_token,
+    access_token_secret:  auth.data.access_token_secret
+  })
+
+  T.get('search/tweets', { q: 'tesla since:2011-07-11', count: 10 }, function(err, data, response) {
+    for(var i = 0; i < data.statuses.length; i++){
+        console.log(data.statuses[i].text)
+    }
+
+  })
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
