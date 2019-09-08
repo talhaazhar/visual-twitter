9ffffd9c30aeb7339989ac594be5be40f736530e
talhaazhar <txa2279@rit.edu>
2019-09-08 10:48:30 -0700

    making twitter info less confusing

--- auth.js
+++ auth.js
@@ -1,13 +1,10 @@
 var auth = {
 	data:{
-		consumer_key: "YOUR-CONSUMER-KEY",
-		consumer_secret: 'YOUR CONSUMER SECRET',
+		consumer_key: "YOUR CONSUMER API KEY",
+		consumer_secret: 'YOUR CONSUMER API SECRET',
 		access_token: 'YOUR TOKEN',
 		access_token_secret: 'YOUR TOKEN SECRET'
 	}
 }
-<<<<<<< HEAD
+
 module.exports = auth;
-=======
-module.exports = auth;
->>>>>>> 64ff0ba64fe8e8ce5a8ae54fe6b8ef1e00ee3fc8
