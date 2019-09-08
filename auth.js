f1350e9bc5866b67e8f0d1ac0bb743e38324b3b4
Talha Azhar <talhaazharr@gmail.com>
2019-09-08 10:53:39 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
 var auth = {
 	data:{
-		consumer_key: "YOUR CONSUMER API KEY",
-		consumer_secret: 'YOUR CONSUMER API SECRET',
-		access_token: 'YOUR TOKEN',
-		access_token_secret: 'YOUR TOKEN SECRET'
+		consumer_key: "YOUR-CONSUMER-API-KEY",
+		consumer_secret: 'YOUR-CONSUMER-API-SECRET',
+		access_token: 'YOUR-TOKEN',
+		access_token_secret: 'YOUR-TOKEN-SECRET'
 	}
 }
 
